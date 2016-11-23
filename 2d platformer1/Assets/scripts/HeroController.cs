fc398ba39eebb0b44ee7cef4ec16e925287f1e6f
Bartozo <B.krol@enet.pl>
2016-11-23 13:05:32 +0100

    drugi commit
    dodano skrypt do poruszania sie postacia oraz skakania.

---
+++ 2d platformer1/Assets/scripts/HeroController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroController : MonoBehaviour {
+
+    public float HeroSpeed;
+    public float HeroJump;
+    private Rigidbody2D rgdbody;
+
+    public Transform GroundCheck;
+    public LayerMask whatIsMask;
+    public float groundCheckRadius;
+       
+    public bool isGrounded;
+	// Use this for initialization
+	void Start () {
+        rgdbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        isGrounded = Physics2D.OverlapCircle(GroundCheck.position, groundCheckRadius, whatIsMask);
+
+
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            rgdbody.velocity = new Vector3(HeroSpeed, rgdbody.velocity.y, 0f);
+        }
+        else if(Input.GetAxisRaw("Horizontal")< 0f)
+        {
+            rgdbody.velocity = new Vector3(-HeroSpeed, rgdbody.velocity.y, 0f);
+        }
+        else
+        {
+            rgdbody.velocity = new Vector3(0f, rgdbody.velocity.y, 0f);
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rgdbody.velocity = new Vector3(rgdbody.velocity.x, HeroJump, 0f);
+        }
+	}
+}
