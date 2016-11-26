0f81862f933d1d38fe4f09a7e666ec377ab904b1
Bartozo <B.krol@enet.pl>
2016-11-26 12:53:14 +0100

    trzeci commit
    naprawiony skrypt chodzenia postaci ,dodano animacje  oraz skrypy kamery

---
+++ 2d platformer1/Assets/scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject target;
+    private Vector3 targetPosition;
+
+    public float followAhead;
+    public float smooth;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPosition = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+
+        if (target.transform.localScale.x > 0f)
+        {
+            targetPosition = new Vector3(targetPosition.x + followAhead, targetPosition.y, targetPosition.z);
+        }
+        else
+        {
+            targetPosition = new Vector3(targetPosition.x - followAhead, targetPosition.y, targetPosition.z);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smooth * Time.deltaTime);
+    }
+}
--- 2d platformer1/Assets/scripts/HeroController.cs
+++ 2d platformer1/Assets/scripts/HeroController.cs
@@ -12,9 +12,11 @@ public class HeroController : MonoBehaviour {
     public float groundCheckRadius;
        
     public bool isGrounded;
+    private Animator myAnimator;
 	// Use this for initialization
 	void Start () {
         rgdbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -26,10 +28,12 @@ public class HeroController : MonoBehaviour {
         if (Input.GetAxisRaw("Horizontal") > 0f)
         {
             rgdbody.velocity = new Vector3(HeroSpeed, rgdbody.velocity.y, 0f);
+            transform.localScale = new Vector3(1f, 1f, 1f);
         }
         else if(Input.GetAxisRaw("Horizontal")< 0f)
         {
             rgdbody.velocity = new Vector3(-HeroSpeed, rgdbody.velocity.y, 0f);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
         }
         else
         {
@@ -40,5 +44,8 @@ public class HeroController : MonoBehaviour {
         {
             rgdbody.velocity = new Vector3(rgdbody.velocity.x, HeroJump, 0f);
         }
+
+        myAnimator.SetFloat("Speed", Mathf.Abs( rgdbody.velocity.x));
+        myAnimator.SetBool("Grounded", isGrounded);
 	}
 }
